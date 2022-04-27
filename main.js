72e71f45be750972f3c2fb1d27172e50d5cfe926
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-04-27 17:43:56 +0200

    create dinamicamente le card del team (tranne le immagini)

---
+++ main.js
@@ -0,0 +1,64 @@
+/*
+Viene fornito un layout di base in cui è presente **una card di esempio** inserita staticamente nell’html. Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e **dovrà quindi essere rimossa dall’html**.
+Aggiungere un file js in cui definire **un array di oggetti** che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: **Nome, Ruolo e Foto**.
+Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell’html, stampare dinamicamente una card per ogni membro del team.
+*/
+
+const teamContainer = document.querySelector('.team-container');
+console.log(teamContainer);
+
+const teamArray = 
+
+[
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: ''
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: ''
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: ''
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: ''
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: ''
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: ''
+  }
+]
+console.log(teamArray);
+
+for(let i in teamArray){
+  const team = teamArray[i];
+  const output = 
+  `
+  <div class="team-card">
+            <div class="card-image">
+              <img
+                src="img/wayne-barnett-founder-ceo.jpg"
+                alt="${team.name}"
+              />
+            </div>
+            <div class="card-text">
+              <h3>${team.name}</h3>
+              <p>${team.role}</p>
+            </div>
+          </div>
+  `;
+  teamContainer.innerHTML += output;
+}
