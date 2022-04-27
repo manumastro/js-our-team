e7b19772c71cc3be2a35aa37e1254b48903f5eb0
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-04-28 00:45:39 +0200

    inserita possibiltà di aggiungere una card tramite il form

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 Viene fornito un layout di base in cui è presente **una card di esempio** inserita staticamente nell’html. Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e **dovrà quindi essere rimossa dall’html**.
 Aggiungere un file js in cui definire **un array di oggetti** che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: **Nome, Ruolo e Foto**.
 Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell’html, stampare dinamicamente una card per ogni membro del team.
+
+Utilizzare gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team: cliccando sul pulsante “add” viene creato un **nuovo oggetto**, il quale viene **inserito nell’array iniziale** e viene stampata una nuova card con tutte le informazioni inserite dall’utente.
+
 */
 
 const teamContainer = document.querySelector('.team-container');
@@ -42,27 +45,76 @@ const teamArray =
   }
 ]
 
-for(let i in teamArray){
-  const team = teamArray[i];
-  team.image = team.name.split(' ').join('-').toLowerCase() + '-' + team.role.split(' ').join('-').toLowerCase() + '.jpg';
+createCards();
 
-  const output = 
-  `
-  <div class="team-card">
-            <div class="card-image">
-              <img
-                src="img/${team.image}""
-                alt="${team.name}"
-              />
-            </div>
-            <div class="card-text">
-              <h3>${team.name}</h3>
-              <p>${team.role}</p>
-            </div>
-          </div>
-  `;
 
-  teamContainer.innerHTML += output;
+document.getElementById('addMemberButton').addEventListener('click', function(){
+
+  const teamArrayAdded = [];
+
+  let newMember = {
+    name: document.getElementById('name').value,
+    role: document.getElementById('role').value,
+    image: document.getElementById('image').value
+  }
+  console.log(newMember);
+  teamArrayAdded.push(newMember);
+
+  createCardsAdded(teamArrayAdded);
+})
+
+function createCards(){
+  for(let i in teamArray){
+    console.log(teamArray[i]);
+  
+    const team = teamArray[i];
+    team.image = team.name.split(' ').join('-').toLowerCase() + '-' + team.role.split(' ').join('-').toLowerCase() + '.jpg';
+  
+    const output = 
+    `
+    <div class="team-card">
+              <div class="card-image">
+                <img
+                  src="img/${team.image}""
+                  alt="${team.name}"
+                />
+              </div>
+              <div class="card-text">
+                <h3>${team.name}</h3>
+                <p>${team.role}</p>
+              </div>
+            </div>
+    `;
+  
+    teamContainer.innerHTML += output;
+  }
+}
+
+function createCardsAdded(teamArrayAdded){
+  for(let i in teamArrayAdded){
+    console.log(teamArrayAdded[i]);
+  
+    const team = teamArrayAdded[i];
+    team.image = team.name.split(' ').join('-').toLowerCase() + '-' + team.role.split(' ').join('-').toLowerCase() + '.jpg';
+  
+    const output = 
+    `
+    <div class="team-card">
+              <div class="card-image">
+                <img
+                  src="img/${team.image}""
+                  alt="${team.name}"
+                />
+              </div>
+              <div class="card-text">
+                <h3>${team.name}</h3>
+                <p>${team.role}</p>
+              </div>
+            </div>
+    `;
+  
+    teamContainer.innerHTML += output;
+  }
 }
 
 
